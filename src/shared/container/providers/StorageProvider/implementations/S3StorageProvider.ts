bc12a514477d150ba73c74edb4dc2956d76f6b6e
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-07 08:57:09 -0300

    fix: change error libs

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,11 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import mime from 'mime';
+const mime = require('mime');
 import aws, { S3 } from 'aws-sdk';
 
 import uploadConfig from '@config/upload';
 import IStorageProvider from 'shared/container/providers/StorageProvider/models/IStorageProvider';
-import upload from '@config/upload';
 
 class S3StorageProvider implements IStorageProvider {
   private client: S3;
@@ -16,35 +15,38 @@ class S3StorageProvider implements IStorageProvider {
     });
   }
 
-  public async saveFile(file: string): Promise<string>{
+  public async saveFile(file: string): Promise<string> {
     const originalPath = path.resolve(uploadConfig.tmpFolder, file);
 
     const ContentType = mime.getType(originalPath);
 
-    if(!ContentType) {
-      throw new  Error('File not found');
+    if (!ContentType) {
+      throw new Error('File not found');
     }
-    const fileContent  = await fs.promises.readFile(originalPath);
+    const fileContent = await fs.promises.readFile(originalPath);
 
-    await this.client.putObject({
-      Bucket: uploadConfig.config.aws.bucket,
-      Key: file,
-      ACL: 'public-read',
-      Body: fileContent,
-      ContentType,
-    }).promise();
+    await this.client
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
 
     await fs.promises.unlink(originalPath);
 
     return file;
   }
 
-
-  public async deleteFile(file: string): Promise<void>{
-    await this.client.deleteObject({
-      Bucket: uploadConfig.config.aws.bucket,
-      Key: file,
-    }).promise();
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+      })
+      .promise();
   }
 }
- export default S3StorageProvider;
+export default S3StorageProvider;
